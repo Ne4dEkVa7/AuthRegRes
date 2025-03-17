c4fff6dd33497d3b7da8f58ceaa2656b7cf4b51c
Kopytovi <kyzmakuzmencko@yandex.ru>
2025-03-17 12:26:56 +0500

    123

--- app/src/main/java/com/example/authregres/fragments/HomeNavigation.kt
+++ app/src/main/java/com/example/authregres/fragments/HomeNavigation.kt
@@ -2,29 +2,47 @@ package com.example.authregres.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.authregres.R
+import com.example.authregres.databinding.HomeNavigationScreenBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
-class HomeNavigation : AppCompatActivity() {
+class HomeNavigation : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding:HomeNavigationScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.home_navigation_screen)
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        val homeFragment = HomeFragment()
-        val profileFragment = ProfileFragment()
-        val settingsFragment = SettingsFragment()
-        setCurrentFragment(homeFragment)
-        bottomNavigationView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> setCurrentFragment(homeFragment)
-                R.id.profile -> setCurrentFragment(profileFragment)
-                R.id.settings -> setCurrentFragment(settingsFragment)
+        binding = HomeNavigationScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        val toggle = ActionBarDrawerToggle(
+            this,binding.drawerLayout,binding.toolbar,R.string.nav_open,R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navigationView.setNavigationItemSelectedListener(this)
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> openFragment(HomeFragment())
+                R.id.profile -> openFragment(ProfileFragment())
+                R.id.cart -> openFragment(CartFragment())
+                R.id.settings -> openFragment(SettingsFragment())
             }
             true
+        }
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+        binding.fab.setOnClickListener(){
+            Toast.makeText(this,"+", Toast.LENGTH_LONG).show()
         }
     }
         private fun setCurrentFragment(fragment: Fragment)=
@@ -32,4 +50,9 @@ class HomeNavigation : AppCompatActivity() {
                 replace(R.id.flFragment, fragment)
                 commit()
             }
+    private fun openFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.flFragment, fragment)
+        fragmentTransaction.commit()
+    }
     }
